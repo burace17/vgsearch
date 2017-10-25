943e60485c4ff1841bb1646789ecdee92ba968e6
Blair Urish <burace17@gmail.com>
2017-10-25 11:36:58 -0500

    Game detail page works now

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -4,23 +4,27 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using vgsearch.Models;
+using Microsoft.Extensions.Logging;
 
 namespace vgsearch.Controllers
 {
     public class GameController : Controller
     {
         private readonly VGDatabaseContext _context;
-        public GameController(VGDatabaseContext context)
+        private readonly ILogger<GameController> _logger;
+        public GameController(VGDatabaseContext context, ILogger<GameController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
-        public IActionResult Index(int? gameId)
+        public async Task<IActionResult> Detail(int? id)
         {
-            if (gameId == null) return NotFound();
+            _logger.LogInformation(1002, "Getting game {ID}", id);
+            if (id == null) return NotFound();
 
             // This will query the database for the game with the specified ID.
-            var game = _context.Games.Where(x => x.id == gameId);
+            var game = await _context.Games.Where(x => x.id == id).SingleOrDefaultAsync();
 
             if (game == null) return NotFound();
 
